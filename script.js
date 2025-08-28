b095d2fc56d70d06d2b964637f16fbbe97c6bd0a
renatanaoki-spec <renata.naoki@gmail.com>
2025-08-28 17:47:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// Simple RSVP alert
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
-  if (form) {
-    form.addEventListener("submit", function (e) {
-      e.preventDefault();
-      alert("Thank you for your RSVP! We can’t wait to see you!");
-    });
-  }
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for your RSVP!");
 });
+
