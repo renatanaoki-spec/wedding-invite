5fae115aa89abceb06b60de907f5817c499f40ac
renatanaoki-spec <renata.naoki@gmail.com>
2025-09-16 20:48:40 +0700

    Delete js directory

--- js/early-guard.js
+++
@@ -1,9 +0,0 @@
-// Redirect ke cover bila belum "dibuka"
-(function(){
-  try{
-    const qp = new URLSearchParams(location.search);
-    if (qp.get('opened') !== '1') {
-      location.replace('cover.html' + (location.search || ''));
-    }
-  }catch(e){}
-})();
